c45cfb72fce84a63df0e43604abd874386d631b7
Basnayake K.S <it24100131@my.sliit.lk>
2026-09-04 11:50:24 +0530

    Delete Minihack member1/TourismApi directory

--- Minihack member1/TourismApi/Models/MongoDbSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace TourismApi.Models
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-    }
-}
